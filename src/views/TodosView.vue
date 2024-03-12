74f2798536768494d1b4ff364d69e24c50a530fd
Chaveguapo <catakeas@gmail.com>
2024-03-12 10:43:02 +0100

    create todo storage

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import TodoCreator from '../components/TodoCreator.vue';
 import { ref } from 'vue';
+import { uid } from 'uid';
+import TodoCreator from '../components/TodoCreator.vue';
 
 const todoList = ref([]);
 
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null
+  });
+}
 
 
 </script>
